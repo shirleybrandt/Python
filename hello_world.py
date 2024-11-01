36281525d45630ef8cc36d183295f12b461a52bc
Shirley Brandt <shirleybrandt@outlook.com>
2024-11-01 12:07:46 +0100

    programmed date

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# print is een functie, een functie wordt altijd gevolgd door haakjes. Je hebt zowel ingebouwde functies als functies die je zelf schrijft
+
+
+# Import date class from datetime module
+from datetime import date
+
+vandaag = date.today()
+
+print('Vandaag is het', vandaag)
