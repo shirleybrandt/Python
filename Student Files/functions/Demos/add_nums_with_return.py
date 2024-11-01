172f99dbdc2795a7aa2829806357085202f72788
Shirley Brandt <shirleybrandt@outlook.com>
2024-11-01 15:29:01 +0100

    demo 2.7 return statement

--- Student Files/functions/Demos/add_nums_with_return.py
+++ Student Files/functions/Demos/add_nums_with_return.py
@@ -1,3 +1,8 @@
+# Demo 2.7
+
+# het return statement wordt hier gebruikt om het print statement 'uit te stellen'
+# het resultaat van add_nums wordt hier 'bewaard' om er later opnieuw iets mee te gaan doen. In dit geval optellen van nummers gebruikmakend van de uitkomst van de eerdere sommen
+
 def add_nums(num1, num2, num3=0, num4=0, num5=0):
     total = num1 + num2 + num3 + num4 + num5
     return total
