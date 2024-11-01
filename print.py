287136c78847f30f03cd4bd7801c21c232045f18
Shirley Brandt <shirleybrandt@outlook.com>
2024-11-01 12:24:53 +0100

    programmed print

---
+++ print.py
@@ -0,0 +1,4 @@
+# de 'sep' named variable geeft aan wat de separator moet zijn tussen de verschillende inputs 
+# de 'end' named variable geeft de opdracht om de de cursur naar de volgende regel te laten verspringen
+
+print('H', 'e', 'l', 'l', 'o', '!', sep = ' ', end='\n')
