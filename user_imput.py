f9cc275b69f82198648832baa813303234abe8d7
Shirley Brandt <shirleybrandt@outlook.com>
2024-11-01 12:29:48 +0100

    programmed user input voorbeeld

---
+++ user_imput.py
@@ -0,0 +1,6 @@
+# we vragen user inpuut. Gebruiken de functie om een vraag door een gebruiker te laten beantwoorden. De vraag wordt beantwoord in de terminal/Python shell
+def main():
+    today = input('wat voor dag is het vandaag? ')
+    print('Wow! Vandaag is het', today)
+
+main()
